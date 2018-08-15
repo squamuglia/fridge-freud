a9f43ff5134834d0a74f616855bfdff425adbb6b
squamuglia <maxsmouha@gmail.com>
2018-08-15 17:20:44 -0400

    draggable elements, building dragHandle

--- src/questions/quietness.js
+++ src/questions/quietness.js
@@ -1,53 +1,48 @@
 import React, { Component } from 'react';
 import PrevNext from '../components/prev-next';
 import { connect } from 'react-redux';
+import Draggable from 'react-draggable';
 
 class Quietness extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dragging: false,
-      pos: {
-        x: 0,
-        y: 0
-      }
+      selected: [],
+      cnt: 0
     };
   }
 
-  mouseDown = e => {
-    this.setState(
-      {
-        dragging: true,
-        pos: {
-          x: e.pageX - this.state.pos.x,
-          y: e.pageY - this.state.pos.y
-        }
-      },
-      () => console.log('down', this.state)
-    );
-  };
+  handleDrag = (e, ui) => {
+    console.log('ui', ui);
+    console.log('id', parseInt(ui.node.id, 10));
+    const id = parseInt(ui.node.id, 10);
+    const newSelected = [...this.state.selected];
 
-  mouseMove = e => {
-    if (!this.state.dragging) {
+    if (ui.y > 90 && !newSelected.includes(id)) {
+      newSelected.push(id);
+      console.log('inside if', newSelected);
       this.setState(
         {
-          pos: {
-            x: e.pageX - this.state.pos.x,
-            y: e.pageY - this.state.pos.y
-          }
+          ...this.state,
+          selected: [...newSelected],
+          cnt: this.state.cnt + 1
         },
-        () => console.log('move', this.state)
+        () => console.log('state', this.state)
+      );
+    } else if (ui.y < 90 && newSelected.includes(id)) {
+      let index = newSelected.indexOf(id);
+      newSelected.slice(index, index + 1);
+      console.log('inside else', newSelected);
+      this.setState(
+        {
+          ...this.state,
+          selected: [...newSelected],
+          cnt: this.state.cnt - 1
+        },
+        () => console.log('state', this.state)
       );
     }
-  };
-
-  mouseUp = e => {
-    this.setState(
-      {
-        dragging: false
-      },
-      () => console.log('up', this.state)
-    );
   };
 
   render() {
@@ -55,16 +50,17 @@ class Quietness extends Component {
       <div className="gutter f fw aic jcc fill abs">
         <div className="fa x m1 card">
           <div className="x">{this.props.question}</div>
-          <p className="s4">Please send him home.</p>
-          <div className="x h-25 bg-light mb1">
+          <p className="s4">Pimp this ride.</p>
+          <div className="x h-25 mb1">
+            <Draggable onDrag={this.handleDrag}>
+              <div key="1" className="sq rel wheel z10" id="1" />
+            </Draggable>
             <div
-              className="red-sq rel"
-              style={{ top: this.state.pos.x, left: this.state.pos.y }}
-              onClick={this.mouseDown}
-              onMouseUp={this.mouseUp}
-              onDrag={this.mouseMove}
+              className="x taurus"
+              style={{ height: '10rem', marginTop: '5rem' }}
             />
           </div>
+          <p>{this.state.cnt}</p>
           <PrevNext />
         </div>
       </div>
