279c36b0f0c74657738943ae4736ced3df06cf3e
squamuglia <maxsmouha@gmail.com>
2018-08-25 20:58:13 -0400

    persist works

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
-import reducer from './reducer/reducer.js';
+import configureStore from './redux/store';
 import App from './App';
-import thunkMiddleware from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(
-  reducer,
-  composeWithDevTools(applyMiddleware(thunkMiddleware))
-);
-
-console.log('index js store', store.getState());
+const { store, persistor } = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
