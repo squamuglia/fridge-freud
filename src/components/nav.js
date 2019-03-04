9e25a728aa4a02a0553e7ec47a28843482c9b91e
squamuglia <maxsmouha@gmail.com>
2019-03-04 11:05:08 -0500

    express backend

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,51 +1,45 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import About from './about';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import About from "./about";
 
 class Nav extends Component {
-  state = {
-    about: false
-  };
+	state = {
+		about: false
+	};
 
-  toggle = state => {
-    if (this.state[state]) {
-      this.setState(
-        {
-          ...this.state,
-          [state]: false
-        },
-        () => console.log(state, this.state)
-      );
-    } else {
-      this.setState(
-        {
-          ...this.state,
-          [state]: true
-        },
-        () => console.log(state, this.state)
-      );
-    }
-  };
+	toggle = state => {
+		if (this.state[state]) {
+			this.setState({
+				...this.state,
+				[state]: false
+			});
+		} else {
+			this.setState({
+				...this.state,
+				[state]: true
+			});
+		}
+	};
 
-  render() {
-    return (
-      <React.Fragment>
-        <About show={this.state.about} toggle={this.toggle} />
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Fridge Freud</Link>
-            </li>
-            <ul>
-              <li key="about" onClick={() => this.toggle('about')}>
-                About
-              </li>
-            </ul>
-          </ul>
-        </nav>
-      </React.Fragment>
-    );
-  }
+	render() {
+		return (
+			<React.Fragment>
+				<About show={this.state.about} toggle={this.toggle} />
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Fridge Freud</Link>
+						</li>
+						<ul>
+							<li key="about" onClick={() => this.toggle("about")}>
+								About
+							</li>
+						</ul>
+					</ul>
+				</nav>
+			</React.Fragment>
+		);
+	}
 }
 
 export default Nav;
